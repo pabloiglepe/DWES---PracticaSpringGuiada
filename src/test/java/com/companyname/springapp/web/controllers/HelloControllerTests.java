a573f4f0d4fd3c83a0bccdf9f261ba8c639089e9
pabloiglepe <pabloiglep2@gmail.com>
2025-11-17 11:14:59 +0100

    Punto 2 - Fallo despliegue

--- src/test/java/com/companyname/springapp/web/controllers/HelloControllerTests.java
+++ src/test/java/com/companyname/springapp/web/controllers/HelloControllerTests.java
@@ -24,6 +24,9 @@ public class HelloControllerTests {
     @Test
     public void testHandleRequestView() {
         ModelAndView modelAndView = controller.handleRequest();
-        assertEquals("hello.jsp", modelAndView.getViewName());
+        assertEquals("hello", modelAndView.getViewName());
+        assertNotNull(modelAndView.getModel());
+        String nowValue = (String) modelAndView.getModel().get("now");
+        assertNotNull(nowValue);
     }
 }
