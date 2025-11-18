3aec06c6c222870e049411993fd22f7673e934fb
pabloiglepe <pabloiglep2@gmail.com>
2025-11-18 14:09:08 +0100

    Punto 4

--- src/main/java/com/companyname/springapp/web/SpringappWebConfig.java
+++ src/main/java/com/companyname/springapp/web/SpringappWebConfig.java
@@ -1,16 +1,24 @@
 package com.companyname.springapp.web;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan
 @EnableWebMvc
 public class SpringappWebConfig {
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("Messages");
+        return messageSource;
+    }
 
     @Bean
     public ViewResolver internalResourceViewResolver() {
